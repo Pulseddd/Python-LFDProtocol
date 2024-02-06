3d4b33bbc81f163f3fb8a9b56c10b118b06c666a
Pulseddd <fortniteclipsfordays45@gmail.com>
2024-02-06 11:20:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ os.system("cls")
 #ic(LFDProtocol.misc.length_to_readable(500000000000000))
 
 
-content_length = LFDProtocol.protocols.PartialContentProtocol("https://server13.mp3quran.net/basit_mjwd/002.mp3").content_length
+content_length = LFDProtocol.protocols.PartialContentProtocol("").content_length
 thread_amt = 20
 a: list = ic(LFDProtocol.misc.divide_ranges(content_length, thread_amt))
 ic(LFDProtocol.misc.length_to_readable(content_length))
